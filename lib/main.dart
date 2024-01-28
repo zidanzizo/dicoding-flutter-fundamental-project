0c4a5d3ed59a433dabe62e38249cc84bb4f7a3b5
zidan <zidan@asaba.co.id>
2024-01-28 13:23:43 +0700

    cupertino app

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App',
+      title: 'News App Cupertino',
       theme: ThemeData(
         colorScheme: Theme.of(context).colorScheme.copyWith(
               primary: primaryColor,
